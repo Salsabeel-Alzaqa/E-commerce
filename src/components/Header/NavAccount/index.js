f5b0d88dd76022cdc8c860fa565c0d8b627a8e6b
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-05-10 23:25:03 +0300

    adding the account settings

---
+++ src/components/Header/NavAccount/index.js
@@ -0,0 +1,68 @@
+import { React, useState } from "react";
+import { createTheme ,Box , Tooltip, Menu, MenuItem, Typography , IconButton} from '@mui/material';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+const settings = [
+    { text: 'Profile', link: '/Profile' },
+    { text: 'Cart', link: '/cart' },
+    { text: 'Logout', link: '/logout' },
+];
+const theme = createTheme({
+        Boxtheme:
+    {
+            display:'flex',
+        borderRadius: '33px',
+            justifyContent: 'center',
+            backgroundColor: 'white',
+            width:'90px',
+        }
+});
+const NavAccount = () => {
+    const [anchorElUser, setAnchorElUser] = useState(null);
+   
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+    };
+    return (
+        <Box sx={theme.Boxtheme}>
+            <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    color="black"
+                >
+                    <PermIdentityIcon />
+                    <ArrowDropDownIcon />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                sx={{ mt: '40px'}}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+            >
+                {settings.map((setting) => (
+                    <MenuItem key={setting.text} onClick={handleCloseUserMenu}>
+                        <Typography textAlign="center">{setting.text}</Typography>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </Box>
+    );
+}
+export default NavAccount;
