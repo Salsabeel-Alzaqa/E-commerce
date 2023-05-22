b6c10dcc8dafbe790ad861af5a50affd22492c66
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-05-22 23:55:52 +0300

    adding layout

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import theme from './theme';
 import { ThemeProvider } from '@mui/material/styles';
@@ -10,9 +9,7 @@ import { ThemeProvider } from '@mui/material/styles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </ThemeProvider>
 );
 // If you want to start measuring performance in your app, pass a function
--- src/route.js
+++ src/route.js
@@ -4,20 +4,22 @@ import {
   Route
 } from "react-router-dom";
 import Login from "./Pages/Login";
+import About from "./Pages/About";
+import Layout from "./Pages/Layout";
 import Guard, { LoginGuard } from "./Pages/Guard";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
+         <Route path="/" element={<Guard><Layout /></Guard>}>
+            <Route index element={
             <Guard>
               <div>Home page</div>
             </Guard>
-          }
-        />
+          } />
+         <Route path="/about" element={<Guard><About /></Guard>} />
+          </Route>
         <Route
           path="/login"
           element={
