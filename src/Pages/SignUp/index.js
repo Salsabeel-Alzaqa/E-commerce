70d042a9193c786db4860be516fcf2d432e55b0e
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-05-23 02:29:56 +0300

    done

--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import Background from '../../components/Backgound';
 import PasswordField from '../../components/PasswordField';
 import axios from "../../axios";
-
 const SignUp = () => {
   const formik = useFormik({
     initialValues: {
@@ -13,21 +12,27 @@ const SignUp = () => {
       email: '',
       password: '',
       confirmPassword: '',
-          phoneNumber: '',
+      phoneNumber: '',
     },
-    validate: (values) => {
+    validate: async (values) => {
       const errors = {};
-
       if (!values.fullName) {
         errors.fullName = 'Full name is required';
       }
-
       if (!values.email) {
         errors.email = 'Email is required';
       } else if (!/\S+@\S+\.\S+/.test(values.email)) {
         errors.email = 'Invalid email address';
+      } else {
+        try {
+          const response = await axios.get(`/users?email=${values.email}`);
+          if (response.data.length > 0) {
+            errors.email = 'Email already exists';
+          }
+        } catch (error) {
+          console.error('Email validation error:', error);
+        }
       }
-
       if (!values.password) {
         errors.password = 'Password is required';
       } else if (values.password.length < 6) {
@@ -36,101 +41,119 @@ const SignUp = () => {
       if (values.password !== values.confirmPassword) {
         errors.confirmPassword = 'Passwords do not match';
       }
-
       if (!values.phoneNumber) {
         errors.phoneNumber = 'Phone number is required';
       }
       return errors;
     },
-    onSubmit: (values) => {
+    onSubmit: async (values , { resetForm }) => {
+      try {
+        const userResponse = await axios.post('/users', {
+          fullName: values.fullName,
+          email: values.email,
+          password: values.password,
+          phone: values.phoneNumber
+        });
+        await Promise.all([
+          axios.post('/carts', {
+            userId: userResponse.data.id,
+            products: [],
+          }),
+          axios.post('/wishlists', {
+            userId: userResponse.data.id,
+            products: [],
+          })
+        ]);
+      resetForm();
+      } catch (error) {
+        console.error('Registration error:', error);
+      }
     },
   });
-      const { handleSubmit, handleChange, handleBlur, values, touched, errors } = formik;
-    return (
-        <Background>
-            <Typography variant="h6" align="center" gutterBottom>
-                Registration
-            </Typography>
-            <Box component="form" onSubmit={handleSubmit}>
-                <TextField
-                    id="fullName"
-                    name="fullName"
-                    label="Full Name"
-                    value={values.fullName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.fullName && Boolean(errors.fullName)}
-                    helperText={touched.fullName && errors.fullName}
-                    size="small"
-                    margin="normal"
-                    fullWidth
-                />
-                <TextField
-                    id="phoneNumber"
-                    name="phoneNumber"
-                    label="Phone Number"
-                    value={values.phoneNumber}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.phoneNumber && Boolean(errors.phoneNumber)}
-                    helperText={touched.phoneNumber && errors.phoneNumber}
-                    size="small"
-                    margin="normal"
-                    fullWidth
-                />
-                <TextField
-                    id="email"
-                    name="email"
-                    label="Email"
-                    type="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.email && Boolean(errors.email)}
-                    helperText={touched.email && errors.email}
-                    size="small"
-                    margin="normal"
-                    fullWidth
-                />
-
-                <PasswordField
-                    id="password"
-                    name="password"
-                    label="Password"
-                    type="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.password && Boolean(errors.password)}
-                    helperText={touched.password && errors.password}
-                    fullWidth
-                    size="small"
-                    margin="normal"
-                />
-
-                <PasswordField
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    label="Confirm Password"
-                    type="password"
-                    value={values.confirmPassword}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.confirmPassword && Boolean(errors.confirmPassword)}
-                    helperText={touched.confirmPassword && errors.confirmPassword}
-                    size="small"
-                    margin="normal"
-                    fullWidth
-                />
-                <Button type="submit" variant="contained" color="primary" fullWidth>
-                    Register
-                </Button>
-            </Box>
-            <Box mb={7} mt={2}>
-                <Typography variant="body2" color="customColor.last">Already a User? <Link to="/login">Login now</Link></Typography>
-            </Box>
-        </Background>
-    );
+  const { handleSubmit, handleChange, handleBlur, values, touched, errors } = formik;
+  return (
+    <Background>
+      <Typography variant="h6" align="center" gutterBottom>
+        Registration
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit}>
+        <TextField
+          id="fullName"
+          name="fullName"
+          label="Full Name"
+          value={values.fullName}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.fullName && Boolean(errors.fullName)}
+          helperText={touched.fullName && errors.fullName}
+          size="small"
+          margin="normal"
+          fullWidth
+        />
+        <TextField
+          id="phoneNumber"
+          name="phoneNumber"
+          label="Phone Number"
+          value={values.phoneNumber}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.phoneNumber && Boolean(errors.phoneNumber)}
+          helperText={touched.phoneNumber && errors.phoneNumber}
+          size="small"
+          margin="normal"
+          fullWidth
+        />
+        <TextField
+          id="email"
+          name="email"
+          label="Email"
+          type="email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.email && Boolean(errors.email)}
+          helperText={touched.email && errors.email}
+          size="small"
+          margin="normal"
+          fullWidth
+        />
+        <PasswordField
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.password && Boolean(errors.password)}
+          helperText={touched.password && errors.password}
+          fullWidth
+          size="small"
+          margin="normal"
+        />
+        <PasswordField
+          id="confirmPassword"
+          name="confirmPassword"
+          label="Confirm Password"
+          type="password"
+          value={values.confirmPassword}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.confirmPassword && Boolean(errors.confirmPassword)}
+          helperText={touched.confirmPassword && errors.confirmPassword}
+          size="small"
+          margin="normal"
+          fullWidth
+        />
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          Register
+        </Button>
+      </Box>
+      <Box mb={7} mt={2}>
+        <Typography variant="body2" color="customColor.last">Already a User? <Link to="/login">Login now</Link></Typography>
+      </Box>
+    </Background>
+  );
 };
 
 export default SignUp;
