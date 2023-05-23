47f5daaf22fd8650a9fa7c88367715628088cc80
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-05-23 20:13:49 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Hero from './components/Hero';
-import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
     <>
-    <Header />
-    <Hero />
+    <Footer />
     </>
   );
 }
