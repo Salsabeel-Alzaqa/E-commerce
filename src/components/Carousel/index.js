7b7f1693382e807729fe28496d8ed76599da2a24
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-05-27 14:55:30 +0300

    update Carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,46 +1,53 @@
-import React, { useRef} from "react";
+import React, { useRef , useState , createContext  } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-// import NextButton from "../NextButton";
-// import PrevButton from "../PrevButton";
 import "./Carousel.css";
-function Carousel({ children , dote , show , initial }) {
+import NextButton from "../NextButton";
+import PrevButton from "../PrevButton";
+export const SliderContext = createContext();
+const Carousel = ({ children, dote, show, initial ,center , Rshow ,Rarrow , arrows}) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
   const customeSlider = useRef();
+   const goToNext = () => {
+    customeSlider.current.slickNext();
+  };
+  const goToPrev = () => {
+    customeSlider.current.slickPrev();
+  };
   const settings = {
     dots: dote,
+    className: "center",
+    centerMode: center,
     slidesToShow: show,
     slidesToScroll: 1,
     infinite: true,
     initialSlide: initial,
+    nextArrow: arrows && <NextButton next={goToNext} />,
+    prevArrow: arrows && <PrevButton prev={goToPrev} />,
+    beforeChange: (current, next) => setCurrentSlide(next),
     responsive: [
       {
-        breakpoint: 1440,
+        breakpoint: 800,
         settings: {
           dots: dote,
-          slidesToShow: show,
+          centerMode: center,
+          slidesToShow:Rshow,
           slidesToScroll: 1,
-          infinite: true
+          infinite: true,
+          nextArrow:Rarrow && <NextButton next={goToNext} />,
+          prevArrow: Rarrow && <PrevButton prev={goToPrev} />,
         }
       }
     ]
   };
-
-  // const gotoNext = () => {
-  //   customeSlider.current.slickNext();
-  // };
-
-  // const gotoPrev = () => {
-  //   customeSlider.current.slickPrev();
-  // };
-
   return (
     <>
-      {/* <NextButton next={gotoNext} />
-      <PrevButton prev={gotoPrev} /> */}
-      <Slider {...settings} ref={customeSlider}>
+      <SliderContext.Provider value={{ currentSlide }}>
+        <Slider {...settings} ref={customeSlider}>
         {children}
-      </Slider>
+          </Slider>
+      </SliderContext.Provider>
     </>
   );
 }
