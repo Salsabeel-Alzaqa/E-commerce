2031bd465ee93493da03bb294f5b6719de280f3b
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-05-28 12:00:47 +0300

    Books Page

--- src/Pages/Books/index.js
+++ src/Pages/Books/index.js
@@ -1,72 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../../axios';
-import { Card, CardMedia, Typography ,Stack , Grid} from '@mui/material';
-import BookImage from './BookImage';
-import Chips from "../../components/Chips";
-import Stars from '../../components/Stars';
-import Description from './Description';
-import BookName from './BookName';
-import Author from './Author';
+import { Box } from '@mui/material';
+import LinkLog from '../../components/LinkLog';
+import BooksList from './BooksList';
+import BooksSkeleton from '../../components/BooksSkeleton';
+import BooksPagination from '../../components/BooksPagination';
 
 function Books() {
   const [loading, setLoading] = useState(true);
   const [books, setBooks] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
   useEffect(() => {
-    axios.get('/books')
-      .then(response => {
-        setBooks(response.data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error(error);
-        setLoading(false);
-      });
-  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('/books');
+      setBooks(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchData();
+}, []);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedBooks = books.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(books.length / itemsPerPage);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div>
+    <>
+      <LinkLog />
+      <Box></Box>
       {loading ? (
-        <div>Loading...</div>
+        Array.from(new Array(5)).map((item,index) => {
+            return (
+              <Box key={index} m={5}>
+                <BooksSkeleton  />
+              </Box>
+            );
+          })
       ) : (
         <>
-          {
-            books.map((items, index) => {
-              return (
-                <Grid container >
-                  <Grid item xs={12} sm={4}>
-                    <BookImage image={items.image} />
-                  </Grid>
-                  <Grid item xs={12} sm={4}>
-                    <Grid container spacing={2}>
-                      <Grid item xs={12}>
-                        <Stack direction="row" spacing={1}>
-                          <Chips text={items.Genre} name="genre"/>
-                          {items.bestseller && <Chips text="best seller" name="bestseller"/>}
-                        </Stack>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <BookName name={items.name} />
-                      </Grid>
-                      <Grid item xs={6} sm={12}>
-                        <Author name={items.author}/>
-                      </Grid>
-                      <Grid item xs={6} sm={12}>
-                        <Stars value={items.rating.rate} count={items.rating.count}  />
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Description desc={items.description} />
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                  <Grid item xs={12} sm={4} >
-                    Item 3
-                  </Grid>
-                </Grid>)
-            })
-          }
+          {displayedBooks.map((item) => {
+            return (
+              <Box key={item.id} m={5}>
+                <BooksList {...item} item={item} />
+              </Box>
+            );
+          })}
+          <Box my={5} display="flex" justifyContent="center">
+          <BooksPagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          /></Box>
         </>
       )}
-    </div>
+    </>
   );
 }
-
 export default Books;
