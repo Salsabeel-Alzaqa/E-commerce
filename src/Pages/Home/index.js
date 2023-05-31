43f45e97ff221bd126011b791349fb0fabccd624
salsabeel alzaqa <107882635+Salsabeel-Alzaqa@users.noreply.github.com>
2023-05-31 18:35:42 +0300

    Update index.js

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Hero from "../../components/Hero";
-import Header from "../../components/Header";
+import Trending from "../../components/Trending";
 function Home() {
     return (
         <>
             <Hero />
+            <Trending />
         </>
     );
 }
