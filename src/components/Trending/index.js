665b21bc67973d36c0e6858310b0894e3ef077f2
salsabeel alzaqa <107882635+Salsabeel-Alzaqa@users.noreply.github.com>
2023-05-31 18:31:02 +0300

    Update Trending

--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
-import Carousel from "../Carousel";
+import TrendingCarousel from "../TrendingCarousel";
 import { Box , Container } from "@mui/material";
 import axios from "../../axios";
 import TrendingCard from "./TrendingCard";
 import Title from "../Title";
-import "./Carousel.css";
 import { styled } from "@mui/system";
+import styles from "./Trending.module.css";
 
 const TrendingContainer = styled(Box)(({ theme }) => ({
   backgroundColor: "#D9D9D9",
   borderRadius: "0 0 180px 180px",
   marginBottom: theme.spacing(5),
 }));
+
 const Trending = () => {
   const [books, setBooks] = useState([]);
     useEffect(() => {
@@ -25,19 +26,20 @@ const Trending = () => {
     };
     fetchBooks();
     }, []);
+  
     const filteredArray = books.filter(obj => obj.trending === true);
-    return (
-        <TrendingContainer>
-            <Title text="TRENDING THIS WEEK" />
-                <Container maxWidth="sm">
-          <Carousel show={3} initial={0} center={true} arrows={true} Rarrow={false} Rshow={3}>
-                        {filteredArray.map((item,index) => (
-                            <Box key={index}>
-                                {item.trending && (<TrendingCard image={item.image} rate={item.rating.rate} index={index} />)}
-                            </Box>))}
-                    </Carousel>
-                </Container>
-        </TrendingContainer>
-    );
+  return (
+    <TrendingContainer>
+      <Title text="TRENDING THIS WEEK" />
+      <Container maxWidth="md">
+        <TrendingCarousel>
+          {filteredArray.map((item, index) => (
+            <Box key={index} className={styles.sliderList}>
+              {item.trending && (<TrendingCard image={item.image} rate={item.rating.rate} index={index} />)}
+            </Box>))}
+        </TrendingCarousel>
+      </Container>
+    </TrendingContainer>
+  );
 }
 export default Trending;
