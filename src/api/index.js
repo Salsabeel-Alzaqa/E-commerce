72c291ecb587720e9ad594790191dcd111a3025f
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-05-31 05:27:25 +0300

    updating

---
+++ src/api/index.js
@@ -0,0 +1,30 @@
+import axios from "../axios";
+
+export const fetchData = async (path,updateData) => {
+  try {
+      const response = await axios.get(path);
+      const data = response.data;
+      updateData(data);
+    }
+     catch (error) {
+    console.error(error);
+    }
+};
+
+export const updateData = async (path, updatedData) => {
+    try {
+        await axios.put(path, updatedData, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const deleteData = async (path) => {
+    try {
+        await axios.delete(path);
+    } catch (error) {
+        console.error(error);
+    }
+};
