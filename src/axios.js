af5af7cb768b12ff76be86d89bb3d98509c7b8f3
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-06-02 03:35:07 +0300

    remove the comments

--- src/axios.js
+++ src/axios.js
@@ -3,15 +3,4 @@ const baseURL = "http://localhost:8000";
 const axiosInstance = axios.create({
   baseURL,
 });
-
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//         config.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
 export default axiosInstance;
