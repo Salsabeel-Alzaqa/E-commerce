32f09479395a4247c291b0d16ee9e201ce94532b
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-06-02 20:48:23 +0300

    add user favorite books

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import LinkLog from "../../components/LinkLog";
 import ProfileInfo from "./ProfileInfo";
+import UserFavorite from "./UserFavorite";
 function Profile() {
     return (
         <>
             <LinkLog />
             <ProfileInfo />
-            {/* <UserFavorite /> */}
+            <UserFavorite />
         </>
     );
 }
